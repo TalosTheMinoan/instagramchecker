f413d2549ae2dbd2a12fef7140f92697a4580a37
TalosTheMinoan <88402424+TalosTheMinoan@users.noreply.github.com>
2023-09-24 19:31:35 +0300

    stealer
    stealer

---
+++ stealaccountpics.py
@@ -0,0 +1,18 @@
+from instaloader import Instaloader, Profile
+
+def download_posts_and_videos(username):
+    L = Instaloader()
+    profile =  Profile.from_username(L.context, username)
+    for post in profile.get_posts():
+        filename = post.date_utc.strftime('%Y=%m-%d %H=%M-%S')
+        if post.is_video:
+            L.download_post(post, target=filename)
+            print(f"Downloaded: {filename}.mp4")
+        else:
+            L.download_post(post, target=filename)
+            print(f"Downloaded: {filename}.jpg")
+
+username = "__mavrikou__" #enter username of target account
+download_posts_and_videos(username)  
+
+
